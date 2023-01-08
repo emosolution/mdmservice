67cd4370d1992d3edc74cee8559e2300e975462c
Thuy Le-Cao <kalickvn@gmail.com>
2023-01-08 23:32:09 +0700

    Upgrade V7

--- src/DMSpro.OMS.MdmService.Domain/MdmServiceDomainModule.cs
+++ src/DMSpro.OMS.MdmService.Domain/MdmServiceDomainModule.cs
@@ -1,10 +1,12 @@
 ﻿using Volo.Abp.Domain;
+using Volo.Abp.Caching;
 using Volo.Abp.Modularity;
 
 namespace DMSpro.OMS.MdmService;
 
 [DependsOn(
     typeof(AbpDddDomainModule),
+    typeof(AbpCachingModule),
     typeof(MdmServiceDomainSharedModule)
 )]
 public class MdmServiceDomainModule : AbpModule
