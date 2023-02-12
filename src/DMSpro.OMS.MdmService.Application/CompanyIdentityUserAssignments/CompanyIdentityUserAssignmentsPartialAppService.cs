991704b8a0a8f135df0290eaee363619d1419331
Thuy Le-Cao <kalickvn@gmail.com>
2023-02-12 22:50:06 +0700

    Update for CompanyIdentityUserAssignment

--- src/DMSpro.OMS.MdmService.Application/CompanyIdentityUserAssignments/CompanyIdentityUserAssignmentsPartialAppService.cs
+++ src/DMSpro.OMS.MdmService.Application/CompanyIdentityUserAssignments/CompanyIdentityUserAssignmentsPartialAppService.cs
@@ -3,6 +3,12 @@ using DMSpro.OMS.MdmService.Companies;
 using Volo.Abp.Caching;
 using Volo.Abp.MultiTenancy;
 using Microsoft.Extensions.Configuration;
+using DevExtreme.AspNet.Data.ResponseModel;
+using DevExtreme.AspNet.Data;
+using DMSpro.OMS.Shared.Domain.Devextreme;
+using DMSpro.OMS.Shared.Lib.Parser;
+using System.Threading.Tasks;
+using System.Linq;
 using DMSpro.OMS.MdmService.Partial;
 
 namespace DMSpro.OMS.MdmService.CompanyIdentityUserAssignments
@@ -31,6 +37,19 @@ namespace DMSpro.OMS.MdmService.CompanyIdentityUserAssignments
 
             _repositories.AddIfNotContains(
                 new KeyValuePair<string, object>("ICompanyRepository", _companyRepository));
+        }
+
+        public override async Task<LoadResult> GetListDevextremesAsync(DataLoadOptionDevextreme inputDev)
+        {
+            var items = await _companyIdentityUserAssignmentRepository.GetQueryAbleForNavigationPropertiesAsync(null);
+            var base_dataloadoption = new DataSourceLoadOptionsBase();
+            DataLoadParser.Parse(base_dataloadoption, inputDev);
+            LoadResult results = DataSourceLoader.Load(items, base_dataloadoption);
+            if (inputDev.Group == null)
+            {
+                results.data = ObjectMapper.Map<IEnumerable<CompanyIdentityUserAssignmentWithNavigationProperties>, IEnumerable<CompanyIdentityUserAssignmentWithNavigationPropertiesDto>>(results.data.Cast<CompanyIdentityUserAssignmentWithNavigationProperties>());
+            }
+            return results;
         }
 	}
 }
