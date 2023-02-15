43a182d94d0a9c203d51132030ff5c502c2ba425
WarBuggy <hovanbuu@gmail.com>
2023-02-16 02:45:02 +0700

    * Add MCPDto and MCPCreateDto

---
+++ src/DMSpro.OMS.MdmService.Application.Contracts/MCPHeaders/MCPCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using DMSpro.OMS.MdmService.MCPDetails;
+using System.Collections.Generic;
+
+namespace DMSpro.OMS.MdmService.MCPHeaders
+{
+    public class MCPCreateDto
+    {
+        public MCPHeaderCreateDto MCPHeaderDto { get; set; }
+        public List<MCPDetailCreateDto> MCPDetails { get; set; } 
+    }
+}
---
+++ src/DMSpro.OMS.MdmService.Application.Contracts/MCPHeaders/MCPDto.cs
@@ -0,0 +1,11 @@
+﻿using DMSpro.OMS.MdmService.MCPDetails;
+using System.Collections.Generic;
+
+namespace DMSpro.OMS.MdmService.MCPHeaders
+{
+    public class MCPDto
+    {
+        public MCPHeaderDto MCPHeaderDto { get; set; }  
+        public List<MCPDetailDto> MCPDetails { get; set; }
+    }
+}
