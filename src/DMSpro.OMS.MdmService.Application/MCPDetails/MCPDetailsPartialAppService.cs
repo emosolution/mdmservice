f035c5dc1c86059b7146d0f3429ac6c9ce731d42
WarBuggy <hovanbuu@gmail.com>
2023-02-16 01:27:21 +0700

    * Make MCPDetail entity ready for import/update excel (regenerate)

--- src/DMSpro.OMS.MdmService.Application/MCPDetails/MCPDetailsPartialAppService.cs
+++ src/DMSpro.OMS.MdmService.Application/MCPDetails/MCPDetailsPartialAppService.cs
@@ -1,42 +1,49 @@
-using DMSpro.OMS.MdmService.Partial;
+using Volo.Abp.Caching;
 using DMSpro.OMS.MdmService.Permissions;
 using Microsoft.AspNetCore.Authorization;
-using Volo.Abp.Caching;
 using Volo.Abp.MultiTenancy;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
-using DMSpro.OMS.MdmService.Customers;
+using DMSpro.OMS.MdmService.Partial;
 using DMSpro.OMS.MdmService.MCPHeaders;
+using DMSpro.OMS.MdmService.Customers;
 
 namespace DMSpro.OMS.MdmService.MCPDetails
 {
-    [Authorize(MdmServicePermissions.MCPDetails.Default)]
-    public partial class MCPDetailsAppService : PartialAppService<MCPDetail, MCPDetailDto, IMCPDetailRepository>, IMCPDetailsAppService
-    {
-        private readonly IMCPDetailRepository _mCPDetailRepository;
-        private readonly IDistributedCache<MCPDetailExcelDownloadTokenCacheItem, string> _excelDownloadTokenCache;
-        private readonly MCPDetailManager _mCPDetailManager;
+	[Authorize(MdmServicePermissions.MCPDetails.Default)]
+	public partial class MCPDetailsAppService : PartialAppService<MCPDetail, MCPDetailDto, IMCPDetailRepository>,
+		IMCPDetailsAppService
+	{
+		private readonly IMCPDetailRepository _mCPDetailRepository;
+		private readonly IDistributedCache<MCPDetailExcelDownloadTokenCacheItem, string>
+			_excelDownloadTokenCache;
+		private readonly MCPDetailManager _mCPDetailManager;
 
-        private readonly ICustomerRepository _customerRepository;
-        private readonly IMCPHeaderRepository _mCPHeaderRepository;
+		private readonly IMCPHeaderRepository _mCPHeaderRepository;
+		private readonly ICustomerRepository _customerRepository;
 
-        public MCPDetailsAppService(ICurrentTenant currentTenant,
-            IMCPDetailRepository repository,
-            MCPDetailManager companyManager,
-            IConfiguration settingProvider,
-            ICustomerRepository customerRepository,
-            IMCPHeaderRepository mCPHeaderRepository,
-            IDistributedCache<MCPDetailExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
-            : base(currentTenant, repository, settingProvider)
-        {
-            _mCPDetailRepository = repository;
-            _excelDownloadTokenCache = excelDownloadTokenCache;
-            _mCPDetailManager = companyManager;
+		public MCPDetailsAppService(ICurrentTenant currentTenant,
+			IMCPDetailRepository repository,
+			MCPDetailManager mCPDetailManager,
+			IConfiguration settingProvider,
+			IMCPHeaderRepository mCPHeaderRepository,
+			ICustomerRepository customerRepository,
+			IDistributedCache<MCPDetailExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
+			: base(currentTenant, repository, settingProvider)
+		{
+			_mCPDetailRepository = repository;
+			_excelDownloadTokenCache = excelDownloadTokenCache;
+			_mCPDetailManager = mCPDetailManager;
+			
+			_mCPHeaderRepository = mCPHeaderRepository;
+			_customerRepository = customerRepository;
 
-            _customerRepository = customerRepository;
-            _mCPHeaderRepository = mCPHeaderRepository;
-            _repositories.Add("ICustomerRepository", _customerRepository);
-            _repositories.Add("IMCPHeaderRepository", _mCPHeaderRepository);
-            _repositories.Add("IMCPDetailRepository", _mCPDetailRepository);
+			_repositories.AddIfNotContains(
+                new KeyValuePair<string, object>("IMCPDetailRepository", _mCPDetailRepository));
+            _repositories.AddIfNotContains(
+                new KeyValuePair<string, object>("IMCPHeaderRepository", _mCPHeaderRepository));
+            _repositories.AddIfNotContains(
+                    new KeyValuePair<string, object>("ICustomerRepository", _customerRepository));
         }
     }
 }
