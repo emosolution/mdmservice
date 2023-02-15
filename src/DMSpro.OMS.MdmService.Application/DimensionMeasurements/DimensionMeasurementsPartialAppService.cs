78ff600bc26cb6141df2c82231a3d410de2a7c6c
WarBuggy <hovanbuu@gmail.com>
2023-02-16 00:37:47 +0700

    * Make DimensionMeasurement entity ready for import/update excel

--- src/DMSpro.OMS.MdmService.Application/DimensionMeasurements/DimensionMeasurementsPartialAppService.cs
+++ src/DMSpro.OMS.MdmService.Application/DimensionMeasurements/DimensionMeasurementsPartialAppService.cs
@@ -1,48 +1,35 @@
-using System.Linq;
+using Volo.Abp.Caching;
+using DMSpro.OMS.MdmService.Permissions;
+using Microsoft.AspNetCore.Authorization;
+using Volo.Abp.MultiTenancy;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using DevExtreme.AspNet.Data;
-using DevExtreme.AspNet.Data.ResponseModel;
-using DMSpro.OMS.Shared.Lib.Parser;
-using DMSpro.OMS.Shared.Domain.Devextreme;
-using Volo.Abp;
-using System.IO;
-using System;
-using Volo.Abp.Content;
+using Microsoft.Extensions.Configuration;
+using DMSpro.OMS.MdmService.Partial;
 
 namespace DMSpro.OMS.MdmService.DimensionMeasurements
 {
-	public partial class DimensionMeasurementsAppService
+	[Authorize(MdmServicePermissions.DimensionMeasurements.Default)]
+	public partial class DimensionMeasurementsAppService : PartialAppService<DimensionMeasurement, DimensionMeasurementDto, IDimensionMeasurementRepository>,
+		IDimensionMeasurementsAppService
 	{
-		public virtual async Task<LoadResult> GetListDevextremesAsync(DataLoadOptionDevextreme inputDev)
-		{
-			var items = await _dimensionMeasurementRepository.GetQueryableAsync();
-			var base_dataloadoption = new DataSourceLoadOptionsBase();
-			DataLoadParser.Parse(base_dataloadoption,inputDev);
-			LoadResult results = DataSourceLoader.Load(items, base_dataloadoption);
-			results.data = ObjectMapper.Map<IEnumerable<DimensionMeasurement>, IEnumerable<DimensionMeasurementDto>>(results.data.Cast<DimensionMeasurement>());
-			return results;
-		}
+		private readonly IDimensionMeasurementRepository _dimensionMeasurementRepository;
+		private readonly IDistributedCache<DimensionMeasurementExcelDownloadTokenCacheItem, string>
+			_excelDownloadTokenCache;
+		private readonly DimensionMeasurementManager _dimensionMeasurementManager;
 
-		public virtual Task<int> UpdateFromExcelAsync(IRemoteStreamContent file)
+		public DimensionMeasurementsAppService(ICurrentTenant currentTenant,
+			IDimensionMeasurementRepository repository,
+			DimensionMeasurementManager dimensionMeasurementManager,
+			IConfiguration settingProvider,
+			IDistributedCache<DimensionMeasurementExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
+			: base(currentTenant, repository, settingProvider)
 		{
-			return null;
+			_dimensionMeasurementRepository = repository;
+			_excelDownloadTokenCache = excelDownloadTokenCache;
+			_dimensionMeasurementManager = dimensionMeasurementManager;
+			
+			_repositories.AddIfNotContains(
+                new KeyValuePair<string, object>("IDimensionMeasurementRepository", _dimensionMeasurementRepository));
 		}
-
-		public virtual async Task<int> InsertFromExcelAsync(IRemoteStreamContent file)
-		{
-			if (file == null || file.ContentLength <= 0) 
-			{
-				throw new BusinessException(message: L["Error:EmptyFormFile"], code: "0");
-			}
-			if (!Path.GetExtension(file.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
-			{
-				throw new BusinessException(message: L["Error:ImportFileNotSupported"], code: "0");
-			}
-			// DUMMY LINE OF CODE TO REMOVE ASYNC AWAIT WARNING
-			await _dimensionMeasurementRepository.GetQueryableAsync(); // to be remove
-
-			return 0;
-		}
-	}
+    }
 }
