b2af38927e4be2d183782abfe688269c9876575f
WarBuggy <hovanbuu@gmail.com>
2023-02-16 01:55:53 +0700

    * Make SalesOrgHierarchy entity ready for import/update excel (regenerate)

--- src/DMSpro.OMS.MdmService.Application/SalesOrgHierarchies/SalesOrgHierarchiesPartialAppService.cs
+++ src/DMSpro.OMS.MdmService.Application/SalesOrgHierarchies/SalesOrgHierarchiesPartialAppService.cs
@@ -1,38 +1,43 @@
-using DMSpro.OMS.MdmService.Partial;
-using DMSpro.OMS.MdmService.Permissions;
-using DMSpro.OMS.MdmService.SalesOrgHeaders;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
 using Volo.Abp.Caching;
+using DMSpro.OMS.MdmService.Permissions;
+using Microsoft.AspNetCore.Authorization;
 using Volo.Abp.MultiTenancy;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using DMSpro.OMS.MdmService.Partial;
+using DMSpro.OMS.MdmService.SalesOrgHeaders;
 
 namespace DMSpro.OMS.MdmService.SalesOrgHierarchies
 {
-    [Authorize(MdmServicePermissions.SalesOrgHierarchies.Default)]
-    public partial class SalesOrgHierarchiesAppService : PartialAppService<SalesOrgHierarchy, SalesOrgHierarchyDto, ISalesOrgHierarchyRepository>,
+	[Authorize(MdmServicePermissions.SalesOrgHierarchies.Default)]
+	public partial class SalesOrgHierarchiesAppService : PartialAppService<SalesOrgHierarchy, SalesOrgHierarchyDto, ISalesOrgHierarchyRepository>,
 		ISalesOrgHierarchiesAppService
-    {
-        private readonly ISalesOrgHierarchyRepository _salesOrgHierarchyRepository;
-        private readonly SalesOrgHierarchyManager _salesOrgHierarchyManager;
-        private readonly IDistributedCache<SalesOrgHierarchyExcelDownloadTokenCacheItem, string> _excelDownloadTokenCache;
+	{
+		private readonly ISalesOrgHierarchyRepository _salesOrgHierarchyRepository;
+		private readonly IDistributedCache<SalesOrgHierarchyExcelDownloadTokenCacheItem, string>
+			_excelDownloadTokenCache;
+		private readonly SalesOrgHierarchyManager _salesOrgHierarchyManager;
 
-        private readonly ISalesOrgHeaderRepository _salesOrgHeaderRepository;
+		private readonly ISalesOrgHeaderRepository _salesOrgHeaderRepository;
 
-        public SalesOrgHierarchiesAppService(ICurrentTenant currentTenant,
-            ISalesOrgHierarchyRepository repository,
-            SalesOrgHierarchyManager manager,
-            IConfiguration settingProvider,
-            ISalesOrgHeaderRepository salesOrgHeaderRepository,
-            IDistributedCache<SalesOrgHierarchyExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
-            : base(currentTenant, repository, settingProvider)
-        {
-            _salesOrgHierarchyRepository = repository;
-            _salesOrgHierarchyManager = manager;
-            _excelDownloadTokenCache = excelDownloadTokenCache;
+		public SalesOrgHierarchiesAppService(ICurrentTenant currentTenant,
+			ISalesOrgHierarchyRepository repository,
+			SalesOrgHierarchyManager salesOrgHierarchyManager,
+			IConfiguration settingProvider,
+			ISalesOrgHeaderRepository salesOrgHeaderRepository,
+			IDistributedCache<SalesOrgHierarchyExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
+			: base(currentTenant, repository, settingProvider)
+		{
+			_salesOrgHierarchyRepository = repository;
+			_excelDownloadTokenCache = excelDownloadTokenCache;
+			_salesOrgHierarchyManager = salesOrgHierarchyManager;
+			
+			_salesOrgHeaderRepository = salesOrgHeaderRepository;
 
-            _salesOrgHeaderRepository = salesOrgHeaderRepository;
-            _repositories.Add("ISalesOrgHeaderRepository", _salesOrgHeaderRepository);
-            _repositories.Add("ISalesOrgHierarchyRepository", _salesOrgHierarchyRepository);
+			_repositories.AddIfNotContains(
+                new KeyValuePair<string, object>("ISalesOrgHierarchyRepository", _salesOrgHierarchyRepository));
+            _repositories.AddIfNotContains(
+                new KeyValuePair<string, object>("ISalesOrgHeaderRepository", _salesOrgHeaderRepository));
         }
     }
 }
