2b08a9df52489e7bab9c4b177a2ad4fc34346b70
WarBuggy <hovanbuu@gmail.com>
2023-02-16 00:48:42 +0700

    * Make GeoMaster entity ready for import/update excel (regenerate)

--- src/DMSpro.OMS.MdmService.Application/GeoMasters/GeoMastersPartialAppService.cs
+++ src/DMSpro.OMS.MdmService.Application/GeoMasters/GeoMastersPartialAppService.cs
@@ -1,31 +1,35 @@
-using System.Collections.Generic;
 using Volo.Abp.Caching;
+using DMSpro.OMS.MdmService.Permissions;
+using Microsoft.AspNetCore.Authorization;
 using Volo.Abp.MultiTenancy;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using DMSpro.OMS.MdmService.Partial;
 
 namespace DMSpro.OMS.MdmService.GeoMasters
 {
-    public partial class GeoMastersAppService : PartialAppService<GeoMaster, GeoMasterDto, IGeoMasterRepository>, 
-        IGeoMastersAppService
-    {
-        private readonly IDistributedCache<GeoMasterExcelDownloadTokenCacheItem, string> _excelDownloadTokenCache;
-        private readonly IGeoMasterRepository _geoMasterRepository;
-        private readonly GeoMasterManager _geoMasterManager;
+	[Authorize(MdmServicePermissions.GeoMasters.Default)]
+	public partial class GeoMastersAppService : PartialAppService<GeoMaster, GeoMasterDto, IGeoMasterRepository>,
+		IGeoMastersAppService
+	{
+		private readonly IGeoMasterRepository _geoMasterRepository;
+		private readonly IDistributedCache<GeoMasterExcelDownloadTokenCacheItem, string>
+			_excelDownloadTokenCache;
+		private readonly GeoMasterManager _geoMasterManager;
 
-        public GeoMastersAppService(ICurrentTenant currentTenant,
-            IGeoMasterRepository repository,
-            GeoMasterManager geoMasterManager,
-            IConfiguration settingProvider,
-            IDistributedCache<GeoMasterExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
-            : base(currentTenant, repository, settingProvider)
-        {
-            _geoMasterRepository = repository;
-            _excelDownloadTokenCache = excelDownloadTokenCache;
-            _geoMasterManager = geoMasterManager;
-
-            _repositories.AddIfNotContains(
+		public GeoMastersAppService(ICurrentTenant currentTenant,
+			IGeoMasterRepository repository,
+			GeoMasterManager geoMasterManager,
+			IConfiguration settingProvider,
+			IDistributedCache<GeoMasterExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
+			: base(currentTenant, repository, settingProvider)
+		{
+			_geoMasterRepository = repository;
+			_excelDownloadTokenCache = excelDownloadTokenCache;
+			_geoMasterManager = geoMasterManager;
+			
+			_repositories.AddIfNotContains(
                 new KeyValuePair<string, object>("IGeoMasterRepository", _geoMasterRepository));
-        }
+		}
     }
 }
