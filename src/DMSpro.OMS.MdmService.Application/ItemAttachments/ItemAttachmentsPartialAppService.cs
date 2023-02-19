a84c02c1a7b86f79dc274e46e85cb7933e608bdc
WarBuggy <hovanbuu@gmail.com>
2023-02-20 03:31:49 +0700

    _ Inject IFileManagementInfoAppService to ItemAttachmentsAppService

--- src/DMSpro.OMS.MdmService.Application/ItemAttachments/ItemAttachmentsPartialAppService.cs
+++ src/DMSpro.OMS.MdmService.Application/ItemAttachments/ItemAttachmentsPartialAppService.cs
@@ -6,35 +6,39 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using DMSpro.OMS.MdmService.Partial;
 using DMSpro.OMS.MdmService.Items;
+using DMSpro.OMS.MdmService.FileManagementInfo;
 
 namespace DMSpro.OMS.MdmService.ItemAttachments
 {
-	[Authorize(MdmServicePermissions.Items.Default)]
-	public partial class ItemAttachmentsAppService : PartialAppService<ItemAttachment, ItemAttachmentDto, IItemAttachmentRepository>,
-		IItemAttachmentsAppService
-	{
-		private readonly IItemAttachmentRepository _itemAttachmentRepository;
-		private readonly IDistributedCache<ItemAttachmentExcelDownloadTokenCacheItem, string>
-			_excelDownloadTokenCache;
-		private readonly ItemAttachmentManager _itemAttachmentManager;
+    [Authorize(MdmServicePermissions.Items.Default)]
+    public partial class ItemAttachmentsAppService : PartialAppService<ItemAttachment, ItemAttachmentDto, IItemAttachmentRepository>,
+        IItemAttachmentsAppService
+    {
+        private readonly IItemAttachmentRepository _itemAttachmentRepository;
+        private readonly IDistributedCache<ItemAttachmentExcelDownloadTokenCacheItem, string>
+            _excelDownloadTokenCache;
+        private readonly ItemAttachmentManager _itemAttachmentManager;
+        private readonly IFileManagementInfoAppService _fileManagementInfoAppService;
 
-		private readonly IItemRepository _itemRepository;
+        private readonly IItemRepository _itemRepository;
 
-		public ItemAttachmentsAppService(ICurrentTenant currentTenant,
-			IItemAttachmentRepository repository,
-			ItemAttachmentManager itemAttachmentManager,
-			IConfiguration settingProvider,
-			IItemRepository itemRepository,
-			IDistributedCache<ItemAttachmentExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
-			: base(currentTenant, repository, settingProvider)
-		{
-			_itemAttachmentRepository = repository;
-			_excelDownloadTokenCache = excelDownloadTokenCache;
-			_itemAttachmentManager = itemAttachmentManager;
-			
-			_itemRepository = itemRepository;
+        public ItemAttachmentsAppService(ICurrentTenant currentTenant,
+            IItemAttachmentRepository repository,
+            ItemAttachmentManager itemAttachmentManager,
+            IFileManagementInfoAppService fileManagementInfoAppService,
+            IConfiguration settingProvider,
+            IItemRepository itemRepository,
+            IDistributedCache<ItemAttachmentExcelDownloadTokenCacheItem, string> excelDownloadTokenCache)
+            : base(currentTenant, repository, settingProvider)
+        {
+            _itemAttachmentRepository = repository;
+            _excelDownloadTokenCache = excelDownloadTokenCache;
+            _itemAttachmentManager = itemAttachmentManager;
+            _fileManagementInfoAppService = fileManagementInfoAppService;
 
-			_repositories.AddIfNotContains(
+            _itemRepository = itemRepository;
+
+            _repositories.AddIfNotContains(
                 new KeyValuePair<string, object>("IItemAttachmentRepository", _itemAttachmentRepository));
             _repositories.AddIfNotContains(
                 new KeyValuePair<string, object>("IItemRepository", _itemRepository));
