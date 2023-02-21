69bfe1ccca3728d4ad1597bfe7e9483f4d1ed8e1
WarBuggy <hovanbuu@gmail.com>
2023-02-21 22:08:44 +0700

    * Add migrations for removing Url and adding FileId

---
+++ src/DMSpro.OMS.MdmService.EntityFrameworkCore/Migrations/20230221150813_RemoveUrlAndAddFileId.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMSpro.OMS.MdmService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUrlAndAddFileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "ItemImages");
+
+            migrationBuilder.DropColumn(
+                name: "url",
+                table: "EmployeeImages");
+
+            migrationBuilder.DropColumn(
+                name: "url",
+                table: "EmployeeAttachments");
+
+            migrationBuilder.DropColumn(
+                name: "url",
+                table: "CustomerAttachments");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FileId",
+                table: "ItemImages",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FileId",
+                table: "EmployeeImages",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FileId",
+                table: "EmployeeAttachments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FileId",
+                table: "CustomerAttachments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "ItemImages");
+
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "EmployeeImages");
+
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "EmployeeAttachments");
+
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "CustomerAttachments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "ItemImages",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "url",
+                table: "EmployeeImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "url",
+                table: "EmployeeAttachments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "url",
+                table: "CustomerAttachments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
