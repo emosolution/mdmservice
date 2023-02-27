7225cf85fbd3d27911918eee9a6ea440716ae5b3
Ho Van Buu <hovanbuu@gmail.com>
2023-02-27 15:20:48 +0700

    * Add migration file

---
+++ src/DMSpro.OMS.MdmService.EntityFrameworkCore/Migrations/20230227082012_AddHierarchyCodeToEmployeeAssignment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMSpro.OMS.MdmService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHierarchyCodeToEmployeeAssignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HierarchyCode",
+                table: "SalesOrgEmpAssignments",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HierarchyCode",
+                table: "SalesOrgEmpAssignments");
+        }
+    }
+}
