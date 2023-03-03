7d76372477cbee62df17cc46ef39a1422ff95c47
WarBuggy <hovanbuu@gmail.com>
2023-03-04 01:54:36 +0700

    * Add partial class for CustomerImage to help with import/update via excel

--- src/DMSpro.OMS.MdmService.Domain/CustomerImages/CustomerImage.cs
+++ src/DMSpro.OMS.MdmService.Domain/CustomerImages/CustomerImage.cs
@@ -1,10 +1,4 @@
-using DMSpro.OMS.MdmService.Customers;
-using DMSpro.OMS.MdmService.Items;
 using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using Volo.Abp.Domain.Entities;
 using Volo.Abp.Domain.Entities.Auditing;
 using Volo.Abp.MultiTenancy;
 using JetBrains.Annotations;
---
+++ src/DMSpro.OMS.MdmService.Domain/CustomerImages/CustomerImagePartial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace DMSpro.OMS.MdmService.CustomerImages
+{
+    public partial class CustomerImage
+    {
+        public Dictionary<string, (int, string, string, string)>
+            GetExcelTemplateInfo()
+        {
+            return new()
+            {
+                { "CustomerId", (0, "ICustomerRepository", "", "") },
+                { "ItemPOSMId", (0, "IItemRepository", "", "") },
+            };
+        }
+
+        public List<string> GetNotNullProperty()
+        {
+            return new();
+        }
+    }
+}
