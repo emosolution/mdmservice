6a42c83c59d954b0e0c5150d3db562c0f8396adc
WarBuggy <hovanbuu@gmail.com>
2023-03-11 03:59:01 +0700

    * Task<SystemDataDto> GetSystemDataByCodeAndValueName(string code, string valueName);
    * Task<List<SystemDataDto>> GetNumberingConfigsSystemData();
    
    * Task<SystemDataDto> GetNumberConfigSystemDataByValueName(string valueName);

--- src/DMSpro.OMS.MdmService.Application.Contracts/SystemDatas/ISystemDatasInternalAppService.cs
+++ src/DMSpro.OMS.MdmService.Application.Contracts/SystemDatas/ISystemDatasInternalAppService.cs
@@ -1,10 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 
 namespace DMSpro.OMS.MdmService.SystemDatas
 {
     public partial interface ISystemDatasInternalAppService : IApplicationService
     {
-        Task<SystemDataDto> GetSystemDataByCodeAndValueNameAsync(string code, string valueName);
+        Task<SystemDataDto> GetSystemDataByCodeAndValueName(string code, string valueName);
+        
+        Task<List<SystemDataDto>> GetNumberingConfigsSystemData();
+
+        Task<SystemDataDto> GetNumberConfigSystemDataByValueName(string valueName);
     }
 }
