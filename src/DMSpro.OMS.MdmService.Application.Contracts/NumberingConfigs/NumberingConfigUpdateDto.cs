86d8c9b440fdd4957b898dbda598808968f927a0
WarBuggy <hovanbuu@gmail.com>
2023-03-12 07:04:34 +0700

    * Add NotNull to NumberingConfigUpdateDto
    * Remove description checking

--- src/DMSpro.OMS.MdmService.Application.Contracts/NumberingConfigs/NumberingConfigUpdateDto.cs
+++ src/DMSpro.OMS.MdmService.Application.Contracts/NumberingConfigs/NumberingConfigUpdateDto.cs
@@ -8,11 +8,12 @@ namespace DMSpro.OMS.MdmService.NumberingConfigs
     public class NumberingConfigUpdateDto : IHasConcurrencyStamp
     {
         [StringLength(NumberingConfigConsts.PrefixMaxLength)]
+        [NotNull]
         public string Prefix { get; set; }
         [StringLength(NumberingConfigConsts.SuffixMaxLength)]
+        [NotNull]
         public string Suffix { get; set; }
         public int PaddingZeroNumber { get; set; }
-        [StringLength(NumberingConfigConsts.DescriptionMaxLength)]
 
         public string ConcurrencyStamp { get; set; }
     }
