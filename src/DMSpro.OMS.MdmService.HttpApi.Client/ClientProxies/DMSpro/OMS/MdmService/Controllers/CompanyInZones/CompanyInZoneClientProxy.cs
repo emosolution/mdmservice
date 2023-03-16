addbaf014346ad0c8a257cc3daf00c41f44bc5fc
Thuy Le-Cao <kalickvn@gmail.com>
2023-03-16 17:43:42 +0700

    Update HTTPCLIENT

---
+++ src/DMSpro.OMS.MdmService.HttpApi.Client/ClientProxies/DMSpro/OMS/MdmService/Controllers/CompanyInZones/CompanyInZoneClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of CompanyInZoneClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace DMSpro.OMS.MdmService.Controllers.CompanyInZones;
+
+public partial class CompanyInZoneClientProxy
+{
+}
---
+++ src/DMSpro.OMS.MdmService.HttpApi.Client/ClientProxies/DMSpro/OMS/MdmService/Controllers/NumberingConfigs/NumberingConfigInternalClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of NumberingConfigInternalClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace DMSpro.OMS.MdmService.Controllers.NumberingConfigs;
+
+public partial class NumberingConfigInternalClientProxy
+{
+}
