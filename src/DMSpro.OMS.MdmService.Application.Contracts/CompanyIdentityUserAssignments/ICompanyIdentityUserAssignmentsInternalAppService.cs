3af049166d0bcd829067e8644d9b6912994677fc
WarBuggy <hovanbuu@gmail.com>
2023-03-24 19:50:29 +0700

    * Create CompanyIdentityUserAssignmentsInternalAppService
    * Add method Task<CompanyDto> GetCurrentlySelectedCompanyAsync(Guid? inputIdentityUserId = null,
                DateTime? checkTime = null) to CompanyIdentityUserAssignmentsInternalAppService
    
    * CompanyIdentityUserAssignmentsAppService uses CompanyIdentityUserAssignmentsInternalAppService to implement GetCurrentlySelectedCompanyAsync
    
    * CompaniesGRPCAppService uses ICompanyIdentityUserAssignmentsInternalAppService to implement GetCurrentlySelectedCompany

---
+++ src/DMSpro.OMS.MdmService.Application.Contracts/CompanyIdentityUserAssignments/ICompanyIdentityUserAssignmentsInternalAppService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using DMSpro.OMS.MdmService.Companies;
+using System;
+
+namespace DMSpro.OMS.MdmService.CompanyIdentityUserAssignments
+{
+    public interface ICompanyIdentityUserAssignmentsInternalAppService
+    {
+        Task<CompanyDto> GetCurrentlySelectedCompanyAsync(
+            Guid? identityUserId = null, DateTime? checkTime = null);
+    }
+}
