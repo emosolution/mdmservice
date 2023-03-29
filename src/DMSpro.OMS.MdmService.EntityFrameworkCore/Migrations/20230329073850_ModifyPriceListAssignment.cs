00e408abad34ee690de736b1ae2591af1dfe5475
Ho Van Buu <hovanbuu@gmail.com>
2023-03-29 14:39:15 +0700

    * Add migration for PriceListAssignment modification

---
+++ src/DMSpro.OMS.MdmService.EntityFrameworkCore/Migrations/20230329073850_ModifyPriceListAssignment.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMSpro.OMS.MdmService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyPriceListAssignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDate",
+                table: "PricelistAssignments",
+                newName: "ReleasedDate");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReleased",
+                table: "PricelistAssignments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReleased",
+                table: "PricelistAssignments");
+
+            migrationBuilder.RenameColumn(
+                name: "ReleasedDate",
+                table: "PricelistAssignments",
+                newName: "ReleaseDate");
+        }
+    }
+}
