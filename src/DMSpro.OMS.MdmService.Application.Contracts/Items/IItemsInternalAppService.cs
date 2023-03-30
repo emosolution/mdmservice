71927efc982a3d7eb99cda27d0e7eda5bb9d374b
WarBuggy <hovanbuu@gmail.com>
2023-03-31 02:04:03 +0700

    * Add ItemsInternalAppService, can be used to check if an item can be updated or not

---
+++ src/DMSpro.OMS.MdmService.Application.Contracts/Items/IItemsInternalAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace DMSpro.OMS.MdmService.Items
+{
+    public interface IItemsInternalAppService : IApplicationService
+    {
+        Task<bool> CheckCanBeUpdatedAsync(Guid id);
+    }
+}
---
+++ src/DMSpro.OMS.MdmService.Application/Items/ItemsInternalAppService.cs
@@ -0,0 +1,78 @@
+﻿using DMSpro.OMS.MdmService.Localization;
+using DMSpro.OMS.Shared.Protos.Shared.Items;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+using Volo.Abp.MultiTenancy;
+
+namespace DMSpro.OMS.MdmService.Items
+{
+    public class ItemsInternalAppService : ApplicationService, IItemsInternalAppService
+    {
+        private readonly IItemsAppService _itemsAppService;
+
+        private readonly IConfiguration _settingProvider;
+        private readonly ICurrentTenant _currentTenant;
+
+        public ItemsInternalAppService(IItemsAppService itemsAppService,
+            IConfiguration settingProvider,
+            ICurrentTenant currentTenant)
+        {
+            _itemsAppService = itemsAppService;
+
+            _settingProvider = settingProvider;
+            _currentTenant = currentTenant;
+
+            LocalizationResource = typeof(MdmServiceResource);
+        }
+
+        public virtual async Task<bool> CheckCanBeUpdatedAsync(Guid id)
+        {
+            var item = await _itemsAppService.GetAsync(id);
+            if (!item.CanUpdate) { return false; }
+
+            CheckItemUsedRequest request = new()
+            {
+                TenantId = _currentTenant.Id.ToString(),
+                ItemId = id.ToString(),
+            };
+            using (GrpcChannel channel =
+                GrpcChannel.ForAddress(_settingProvider["GrpcRemotes:OrderServiceUrl"]))
+            {
+                var client =
+                    new ItemsProtoAppService.ItemsProtoAppServiceClient(channel);
+               
+                var response = await client.CheckItemUsedInOrderServiceAsync(request);
+                if (!response.Success)
+                {
+                    throw new UserFriendlyException(message: response.Error, code: "1");
+                }
+                if (response.ItemUsed)
+                {
+                    return false;
+                }
+            }
+
+            using (GrpcChannel channel =
+                GrpcChannel.ForAddress(_settingProvider["GrpcRemotes:InventoryServiceUrl"]))
+            {
+                var client =
+                    new ItemsProtoAppService.ItemsProtoAppServiceClient(channel);
+                var response = await client.CheckItemUsedInInventoryServiceAsync(request);
+                if (!response.Success)
+                {
+                    throw new UserFriendlyException(message: response.Error, code: "1");
+                }
+                if (response.ItemUsed)
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
