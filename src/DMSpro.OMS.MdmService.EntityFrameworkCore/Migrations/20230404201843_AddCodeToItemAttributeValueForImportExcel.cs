d0726f40fcc386b349263154685b9bd3730f38cd
WarBuggy <hovanbuu@gmail.com>
2023-04-05 03:20:40 +0700

    * Add migration files

---
+++ src/DMSpro.OMS.MdmService.EntityFrameworkCore/Migrations/20230404201843_AddCodeToItemAttributeValueForImportExcel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMSpro.OMS.MdmService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeToItemAttributeValueForImportExcel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "ItemAttributeValues",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "ItemAttributeValues");
+        }
+    }
+}
