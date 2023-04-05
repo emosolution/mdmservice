1bfd8edc17ca9ae644e8c7579289090f0485f814
WarBuggy <hovanbuu@gmail.com>
2023-04-06 04:09:46 +0700

    * Add repo methods for entity with code to IItemAttributeValueRepository

--- src/DMSpro.OMS.MdmService.Domain/ItemAttributeValues/IItemAttributeValuePartialRepository.cs
+++ src/DMSpro.OMS.MdmService.Domain/ItemAttributeValues/IItemAttributeValuePartialRepository.cs
@@ -7,6 +7,14 @@ namespace DMSpro.OMS.MdmService.ItemAttributeValues
 {
     public partial interface IItemAttributeValueRepository : IRepository<ItemAttributeValue, Guid>
     {
+        Task<Guid?> GetIdByCodeAsync(string code);
+
+        Task<Dictionary<string, Guid>> GetListIdByCodeAsync(List<string> codes);
+
+        Task<int> GetCountByCodeAsync(List<string> codes);
+
+        Task<bool> CheckUniqueCodeForUpdate(List<string> codes, List<Guid> ids);
+
         Task<List<ItemAttributeValue>> GetByIdAsync(List<Guid> ids);
     }
 }
