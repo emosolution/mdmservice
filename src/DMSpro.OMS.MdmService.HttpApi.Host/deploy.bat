1f22709ca4accca36f3f83361419e49c3bdffa91
WarBuggy <hovanbuu@gmail.com>
2023-04-09 02:56:27 +0700

    * Fix bug in deploy.bat

--- src/DMSpro.OMS.MdmService.HttpApi.Host/deploy.bat
+++ src/DMSpro.OMS.MdmService.HttpApi.Host/deploy.bat
@@ -1,12 +1,17 @@
-@echo off
 
+
+set projectRoot=c:\code\redant\
 set microServiceFullName=MdmService
 set commondllName=mdm
 set microServicePath=src\DMSpro.OMS.%microServiceFullName%.
+set microServiceFullPath=%projectRoot%%microServiceFullName%\%microServicePath%
 set commondllPath=..\commondll\Development\Services\%commondllName%\
 set deployedPath=C:\Code\redant\deploydmspro\
-set microServiceDeployFolder=services\MDMService\
+set deployFolder=MDMService
+set microServiceDeployFolder=services\%deployFolder%\
 set fullDeployPath=%deployedPath%%microServiceDeployFolder%
+set commiter=hovanbuu@gmail.com
+set gitCommitMessage="Update and deploy %microServiceFullName% by %commiter%"
 
 dotnet clean
 
@@ -26,7 +31,25 @@ copy %microServicePath%HttpApi.Client\bin\Debug\netstandard2.0\*.HttpApi.Client.
 
 copy %microServicePath%Web\bin\Debug\net7.0\*.Web.dll %commondllPath%
 
-cd %microServicePath%HttpApi.Host
+cd %fullDeployPath%
+
+cd ..
+
+RD "%deployFolder%" /S /Q
+
+mkdir %deployFolder%
+
+cd %microServiceFullPath%HttpApi.Host
 
 dotnet publish -c Release -o %fullDeployPath%
+
+cd %fullDeployPath%
+
+git add .
+
+git commit -m %gitCommitMessage%
+
+git push
+
+cd %microServiceFullPath%HttpApi.Host
 
