ad2dc7803d9bd647127470ff8549079aeab79f9b
WarBuggy <hovanbuu@gmail.com>
2023-04-11 00:45:04 +0700

    * Add EmployeeTypes enum

---
+++ src/DMSpro.OMS.MdmService.Domain.Shared/EmployeeProfiles/EmployeeTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace DMSpro.OMS.MdmService.EmployeeProfiles
+{
+    public enum EmployeeTypes
+    {
+        Salesman = 0,
+        Deliveryman = 1,
+        Supervisor = 2,
+        PromotionGirl = 3,
+    }
+}
