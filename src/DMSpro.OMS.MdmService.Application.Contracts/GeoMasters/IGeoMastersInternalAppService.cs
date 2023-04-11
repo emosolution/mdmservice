88f47e3a1beb206d34d812fe72f9803c5c717271
WarBuggy <hovanbuu@gmail.com>
2023-04-12 02:44:44 +0700

    * Add localization to CompanyIdentityUserAssignments
    * Add IGeoMastersInternalAppService
    
    * Add method to check 5 geo input in IGeoMastersInternalAppService

---
+++ src/DMSpro.OMS.MdmService.Application.Contracts/GeoMasters/IGeoMastersInternalAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using static DMSpro.OMS.MdmService.Permissions.MdmServicePermissions;
+
+namespace DMSpro.OMS.MdmService.GeoMasters
+{
+    public interface IGeoMastersInternalAppService : IApplicationService
+    {
+        Task<(
+            GeoMasterDto, GeoMasterDto, GeoMasterDto, GeoMasterDto, GeoMasterDto
+            )> CheckAllGeoInputs(Guid? geo0Id, Guid? geo1Id, Guid? geo2Id, Guid? geo3Id, Guid? geo4Id);
+    }
+}
