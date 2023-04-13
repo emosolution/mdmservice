19b11a02532fc3cf73613f5f8ef0506dc9eff6f7
Ho Van Buu <hovanbuu@gmail.com>
2023-04-13 14:53:18 +0700

    * Remove CompanyId from CompanyIdentityUserAssignmentUpdateDto

--- src/DMSpro.OMS.MdmService.Application.Contracts/CompanyIdentityUserAssignments/CompanyIdentityUserAssignmentUpdateDto.cs
+++ src/DMSpro.OMS.MdmService.Application.Contracts/CompanyIdentityUserAssignments/CompanyIdentityUserAssignmentUpdateDto.cs
@@ -8,7 +8,6 @@ namespace DMSpro.OMS.MdmService.CompanyIdentityUserAssignments
     public class CompanyIdentityUserAssignmentUpdateDto : IHasConcurrencyStamp
     {
         public Guid IdentityUserId { get; set; }
-        public Guid CompanyId { get; set; }
 
         public string ConcurrencyStamp { get; set; }
     }
