4c5d8d9421e2f72758de0e880c0f7ce1b6f6206e
Ho Van Buu <hovanbuu@gmail.com>
2023-04-19 10:45:40 +0700

    * Remove unused imports
    * Remove async and Task keywords in OnGetAsync in public class LookupModal : MdmServicePageModel
    
    * Remove unused references in Application.Contracts.csproj

--- src/DMSpro.OMS.MdmService.Web/Pages/Shared/LookupModal.cshtml.cs
+++ src/DMSpro.OMS.MdmService.Web/Pages/Shared/LookupModal.cshtml.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace DMSpro.OMS.MdmService.Web.Pages.Shared
 {
     public class LookupModal : MdmServicePageModel
@@ -9,7 +5,7 @@ namespace DMSpro.OMS.MdmService.Web.Pages.Shared
         public string CurrentId { get; set; }
         public string CurrentDisplayName { get; set; }
 
-        public async Task OnGetAsync(string currentId, string currentDisplayName)
+        public void OnGetAsync(string currentId, string currentDisplayName)
         {
             CurrentId = currentId;
             CurrentDisplayName = currentDisplayName;
